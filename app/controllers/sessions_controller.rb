c1b96b0029a0c97eafcd6e7199f91423ea5a98cd
brown0702 <brown070288@gmail.com>
2014-07-23 00:43:26 +0900

    updateを押下時にtokenが切れている場合の問題をfix しかし、noticeが出ないようになってしまった

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,10 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    if user.token != auth['credentials']['token']
+      user.token = auth['credentials']['token']
+      user.save
+    end
 
     session[:user_id] = user.id
     redirect_to users_url
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    redirect_to '/auth/facebook'
     save_fb_data
-    redirect_to '/users', :notice => 'Facebookからデータを取得しました！'
+    # redirect_to '/users', :notice => 'Facebookからデータを取得しました！'
   end
 
   def gender
